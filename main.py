db691b0c2206f5789936628b54d983ce40058579
Weijing Wang <weijingrocks@gmail.com>
2020-09-26 12:14:23 -0700

    more rooms

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 from fadein import *
+from displayText import *
 pygame.mixer.pre_init()
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
@@ -214,7 +215,7 @@ class level():
                 self.current_level_walls_exits = [[0,"wall"],[800,"exit"],[0,"wall"],[520,"exit"]]
 
             elif self.background_count_x==1 and self.background_count_y==1:
-                self.current_background = pygame.image.load("./assets/y1.png")
+                self.current_background = pygame.image.load("./assets/x1y1.png")
                 self.current_background_count_x=1
                 self.current_background_count_y=1
                 self.current_level_walls_exits = [[0,"exit"],[800,"exit"],[0,"exit"],[520,"exit"]]
@@ -229,8 +230,17 @@ class level():
                 self.current_background = pygame.image.load("./assets/x1y2.png")
                 self.current_background_count_x=1
                 self.current_background_count_y=2
-                self.current_level_walls_exits = [[0,"exit"],[800,"exit"],[0,"exit"],[520,"exit"]]
-
+                self.current_level_walls_exits = [[0,"wall"],[800,"wall"],[0,"exit"],[520,"exit"]]
+            elif self.background_count_x==1 and self.background_count_y==3:
+                self.current_background = pygame.image.load("./assets/x1y3.png")
+                self.current_background_count_x=1
+                self.current_background_count_y=3
+                self.current_level_walls_exits = [[0,"wall"],[800,"exit"],[0,"exit"],[520,"exit"]]
+            elif self.background_count_x==2 and self.background_count_y==3:
+                self.current_background = pygame.image.load("./assets/x2y3.png")
+                self.current_background_count_x=2
+                self.current_background_count_y=3
+                self.current_level_walls_exits = [[0,"exit"],[800,"exit"],[0,"wall"],[520,"wall"]]
 
             return self.current_level_walls_exits
 
@@ -266,6 +276,14 @@ class level():
 def render_all_objects(level_info,object_info,playerX,playerY,playerW,playerH):
     background_x = level_info[0]
     background_y = level_info[1]
+    do_diologue = False
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            quit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                goatVoice()
+                do_diologue = True
     if background_x == 0 and background_y == 0:
         #look at how many objects there are in object_info
         for goat in object_info[0]:
@@ -274,33 +292,77 @@ def render_all_objects(level_info,object_info,playerX,playerY,playerW,playerH):
             draw_goat = myObject(goat[0],goat[1],goat[2],goat[3],goat[4],goat[5],goat[6])
             draw_goat.draw()
             draw_goat.collision_check(playerX,playerY,playerW,playerH)
+            if do_diologue ==True:
+                gameIntro(screen,"goat")
+                do_diologue= False
         #create objects based off given info in list with object class
+
     elif background_x == 1 and background_y == 0:
         for goat in object_info[1]:
             draw_goat = myObject(goat[0],goat[1],goat[2],goat[3],goat[4],goat[5],goat[6])
             draw_goat.draw()
             draw_goat.collision_check(playerX,playerY,playerW,playerH)
+
+            if do_diologue ==True:
+                gameIntro(screen,"first")
+                do_diologue= False
+
     elif background_x == 2 and background_y == 0:
         for goat in object_info[2]:
             draw_goat = myObject(goat[0],goat[1],goat[2],goat[3],goat[4],goat[5],goat[6])
             draw_goat.draw()
             draw_goat.collision_check(playerX,playerY,playerW,playerH)
+            if do_diologue ==True:
+                gameIntro(screen,"goat")
+                do_diologue= False
     elif background_x == 0 and background_y == 1:
         for goat in object_info[3]:
             draw_goat = myObject(goat[0],goat[1],goat[2],goat[3],goat[4],goat[5],goat[6])
             draw_goat.draw()
             draw_goat.collision_check(playerX,playerY,playerW,playerH)
+            if do_diologue ==True:
+                gameIntro(screen,"goat")
+                do_diologue= False
     elif background_x == 1 and background_y == 1:
         for goat in object_info[4]:
             draw_goat = myObject(goat[0],goat[1],goat[2],goat[3],goat[4],goat[5],goat[6])
             draw_goat.draw()
             draw_goat.collision_check(playerX,playerY,playerW,playerH)
+            if do_diologue ==True:
+                gameIntro(screen,"ethan")
+                do_diologue= False
     elif background_x == 2 and background_y == 1:
         for goat in object_info[5]:
             draw_goat = myObject(goat[0],goat[1],goat[2],goat[3],goat[4],goat[5],goat[6])
             draw_goat.draw()
             draw_goat.collision_check(playerX,playerY,playerW,playerH)
-
+            if do_diologue ==True:
+                gameIntro(screen,"goat")
+                do_diologue= False
+    elif background_x == 1 and background_y == 2:
+        for goat in object_info[6]:
+            draw_goat = myObject(goat[0],goat[1],goat[2],goat[3],goat[4],goat[5],goat[6])
+            draw_goat.draw()
+            draw_goat.collision_check(playerX,playerY,playerW,playerH)
+            if do_diologue ==True:
+                gameIntro(screen,"monk")
+                do_diologue= False
+    elif background_x == 1 and background_y == 3:
+        for goat in object_info[7]:
+            draw_goat = myObject(goat[0],goat[1],goat[2],goat[3],goat[4],goat[5],goat[6])
+            draw_goat.draw()
+            draw_goat.collision_check(playerX,playerY,playerW,playerH)
+            if do_diologue ==True:
+                gameIntro(screen,"second")
+                do_diologue= False
+    elif background_x == 2 and background_y == 3:
+        for goat in object_info[8]:
+            draw_goat = myObject(goat[0],goat[1],goat[2],goat[3],goat[4],goat[5],goat[6])
+            draw_goat.draw()
+            draw_goat.collision_check(playerX,playerY,playerW,playerH)
+            if do_diologue ==True:
+                gameIntro(screen,"second")
+                do_diologue= False
     else:
         pass
 
@@ -326,7 +388,132 @@ def stillScene(image,x,y,button):
 			screen.blit(image,(x,y))
 			pygame.display.flip()
 
+def gameIntro(surface,which_diologue):
+    done = False
+    pictureCount = 0
+    sayWhat = None
+    person = face_player
+    skip = False
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    pictureCount +=1
+                elif event.key == pygame.K_RETURN:
+                    skip = True
+            if which_diologue == "first":
+                if pictureCount == 0:
+                    sayWhat = "where where am i?! hello hello"
+                elif pictureCount == 1:
+                    person = face_colonel
+                    sayWhat = "over over i cant find you on the gps whe-"
+                elif pictureCount == 2:
+                    person = face_player
+                    sayWhat = "hey guys hey guys hey guys HEEEEYYY GUYYYYSSS"
+                elif pictureCount == 3:
+                    person = goat
+                    goatVoice()
+                    sayWhat = 'baaa'
+                elif pictureCount ==4:
+                    person = face_player
+                    sayWhat = 'shut up'
+                if skip == True or pictureCount > 4:
+                    done = True
+                
+            if which_diologue == "goat":
+                if pictureCount == 0:
+                    sayWhat = random.choice(["well hello there","i hate goats","hey guys","punch goat","AADWDWAAWDHWOAHDIQ*@","(*#@$Y(@*#$(@#$&","hi friend."])
+                elif pictureCount == 1:
+                    person = goat
+                    goatVoice()
+                    sayWhat = random.choice(["baaaa","meeeee","hey guys",".............","I AM SHEEP"])
+                elif pictureCount == 2:
+                    person = face_player
+                    sayWhat = random.choice(["hey guys hey guys hey guys HEEEEYYY GUYYYYSSS","are you a gamer","how do i get out","im gonna eat you","oh no i have to poo","what is your secret?", "what is the secret of the island?"])
+                elif pictureCount == 3:
+                    person = goat
+                    goatVoice()
+                    sayWhat = random.choice(["hey guys hey guys hey guys","baaaaaaa","meee","..........","go away"])
+                elif pictureCount ==4:
+                    person = face_player
+                    sayWhat = 'shut up'
+                if skip == True or pictureCount > 4:
+                    done = True
 
+            if which_diologue == "ethan":
+                if pictureCount == 0:
+                    sayWhat = "A food source. finally"
+                elif pictureCount == 1:
+                    person = goat
+                    goatVoice()
+                    sayWhat = "i am not your food, i am your salvation"
+                elif pictureCount == 2:
+                    person = face_player
+                    sayWhat = "wtf"
+                elif pictureCount == 3:
+                    person = goat
+                    goatVoice()
+                    sayWhat = "I shall be the messiah that brings u to the next level"
+                elif pictureCount ==4:
+                    person = face_player
+                    sayWhat = "ok bro ima eat yo kids-player"
+                elif pictureCount ==5:
+                    person = goat
+                    sayWhat = "i will become your-"
+                elif pictureCount ==6:
+                    person = face_player
+                    sayWhat = "shut up"
+                if skip == True or pictureCount > 6:
+                    done = True
+
+            if which_diologue == "monk":
+                if pictureCount == 0:
+                    sayWhat = "what is this!"
+                elif pictureCount == 1:
+                    person = monk
+                    goatVoice()
+                    sayWhat = "........"
+                elif pictureCount == 2:
+                    person = face_player
+                    sayWhat = "????"
+                elif pictureCount == 3:
+                    person = monk
+                    sayWhat = "...."
+                elif pictureCount ==4:
+                    person = face_player
+                    sayWhat = "ok bye"
+                if skip == True or pictureCount > 4:
+                    done = True
+
+            if which_diologue == "second":
+                if pictureCount == 0:
+                    person = face_colonel
+                    sayWhat = "where are you! over"
+                elif pictureCount == 1:
+                    person = face_player
+                    sayWhat = "apparentally i am on an island at the moment"
+                elif pictureCount == 2:
+                    sayWhat = "except there are goats everywhere"
+                elif pictureCount == 3:
+                    person = face_colonel
+                    sayWhat = "i-"
+                elif pictureCount ==4:
+                    person = face_player
+                    sayWhat = "EVERYWHERE damn...."
+                elif pictureCount ==5:
+                    sayWhat = "hello? hello? over over"
+                    goatVoice()
+                if skip == True or pictureCount > 5:
+                    done = True
+
+        pygame.draw.rect(surface, (0,0,0), pygame.Rect(0, 500, 800, 100))
+
+
+        surface.blit(pygame.transform.scale(person, (100, 100)), (0, 500))
+        messageText(sayWhat,100,550,20,surface,255,255,255,"Roboto")
+        pygame.display.update()
 
 
 
@@ -336,9 +523,13 @@ title = pygame.image.load(("./assets/title.png"))
 intro1 = pygame.image.load(("./assets/intro1.png"))
 intro2 = pygame.image.load(("./assets/intro2.png"))
 intro3 = pygame.image.load(("./assets/intro3.png"))
-
 game_start = pygame.image.load(("./assets/game_start.png"))
+
 goat = pygame.image.load("./assets/goat.png")
+monk = pygame.image.load("./assets/monk.png")
+walkie_talkie = pygame.image.load("./assets/walkie_talkie.png")
+face_colonel = pygame.image.load("./assets/face_colonel.png")
+face_player = pygame.image.load("./assets/face_player.png")
 #music
 pygame.mixer.music.load("./assets/sounds/poo_short.mp3") 
 pygame.mixer.music.play(-1,0.0)
@@ -358,39 +549,49 @@ me = player(100,100,185,400,100,screen)
 my_objects = [
     #0,0
     [
-        [600,150,75,82,"poo",goat,screen],
-        [50,200,75,82,"poo",goat,screen],
-        [330,400,75,82,"poo",goat,screen]
+        [600,150,150,164,"poo",goat,screen],
+        [50,200,150,164,"poo",goat,screen],
+        [330,400,150,164,"poo",goat,screen]
         ],
     #1,0
     [
-        [500,200,75,82,"poo",goat,screen],
-        [100,400,75,82,"poo",goat,screen]
+        [500,200,150,164,"poo",goat,screen],
+        [100,400,150,164,"poo",goat,screen]
         ],
     #2,0
     [
-        [400,200,75,82,"poo",goat,screen],
-        [100,400,75,82,"poo",goat,screen],
-        [300,500,75,82,"poo",goat,screen]
+        [400,200,150,164,"poo",goat,screen],
+        [100,400,150,164,"poo",goat,screen],
+        [300,500,150,164,"poo",goat,screen]
         ],
     #0,1
     [
-        [400,100,75,82,"poo",goat,screen],
-        [100,400,75,82,"poo",goat,screen],
-        [300,500,75,82,"poo",goat,screen]
+        [400,100,150,164,"poo",goat,screen],
+        [100,400,150,164,"poo",goat,screen],
+        [300,500,150,164,"poo",goat,screen]
         ],
     #1,1
     [
-        [200,300,75,82,"poo",goat,screen],
+        [200,300,150,164,"poo",goat,screen],
         ],
     #2,1
     [
-        [50,150,75,82,"poo",goat,screen],
-        [100,400,75,82,"poo",goat,screen],
-        [300,500,75,82,"poo",goat,screen],
-        [600,200,75,82,"poo",goat,screen]
+        [50,150,150,164,"poo",goat,screen],
+        [100,400,150,164,"poo",goat,screen],
+        [300,500,150,164,"poo",goat,screen],
+        [600,200,150,164,"poo",goat,screen]
         ],
-
+    #1,2
+    [
+        [50,150,299,343,"poo",monk,screen],
+        ],
+    #1,3
+    [
+        [0,180,200,100,"poo",walkie_talkie,screen],
+        ],
+    #2,3
+    [
+        ],
 ]
 def main_game():
     done = False
@@ -417,14 +618,19 @@ def main_game():
         clock.tick(60)
         pygame.display.flip()
 
-fadein(title,screen,1,False)
-#intro story
-stillScene(intro1,0,0,pygame.K_SPACE)
-stillScene(intro2,0,0,pygame.K_SPACE)
-stillScene(intro3,0,0,pygame.K_SPACE)
 
-fadein(game_start,screen,2,True)
 
+
+
+
+
+# fadein(title,screen,1,False)
+# stillScene(intro1,0,0,pygame.K_SPACE)
+# stillScene(intro2,0,0,pygame.K_SPACE)
+# stillScene(intro3,0,0,pygame.K_SPACE)
+# fadein(game_start,screen,2,True)
 main_game()
+
+
 
 
